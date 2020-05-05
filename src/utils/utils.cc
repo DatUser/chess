f3ea9c760b0b792e659e549f6370707e89941295
clement.leconte <clement.leconte@epita.fr>
2020-05-05 17:58:19 +0200

    [NEW] src/utils [FIX] src/utils
    [NEW] added a method that convert int coordinates (0-7, 0-7) to its position
    [FIX] fix the use of signed long long to unsigned long long so we can have
    the 2^63 available

--- src/utils/utils.cc
+++ src/utils/utils.cc
@@ -1,10 +1,76 @@
 #include "utils.hh"
 
 namespace utils {
-    long long int two_pow(int power) {
-        long long int res = 1;
+    unsigned long long int two_pow(int power) {
+        unsigned long long int res = 1;
         for (int i = 0; i < power; i++) {
             res *= 2;
         } return res;
+    }
+
+    int pow_two(unsigned long long int number) {
+        int res = 0;
+        while (number > 1) {
+            number /= 2;
+            res++;
+        } return res;
+    }
+
+
+    board::Position to_position(int x, int y) {
+        board::File file;
+        board::Rank rank;
+
+        switch (x) {
+            case 0:
+                file = board::File::A;
+                break;
+            case 1:
+                file = board::File::A;
+                break;
+            case 2:
+                file = board::File::A;
+                break;
+            case 3:
+                file = board::File::A;
+                break;
+            case 4:
+                file = board::File::A;
+                break;
+            case 5:
+                file = board::File::A;
+                break;
+            case 6:
+                file = board::File::A;
+                break;
+            case 7:
+                file = board::File::A;
+                break;
+        } switch (y) {
+            case 0:
+                rank = board::Rank::ONE;
+                break;
+            case 1:
+                rank = board::Rank::TWO;
+                break;
+            case 2:
+                rank = board::Rank::THREE;
+                break;
+            case 3:
+                rank = board::Rank::FOUR;
+                break;
+            case 4:
+                rank = board::Rank::FIVE;
+                break;
+            case 5:
+                rank = board::Rank::SIX;
+                break;
+            case 6:
+                rank = board::Rank::SEVEN;
+                break;
+            case 7:
+                rank = board::Rank::EIGHT;
+                break;
+        } return board::Position(file, rank);
     }
 }
--- src/utils/utils.hh
+++ src/utils/utils.hh
@@ -1,7 +1,11 @@
 #pragma once
 
+#include "../all.hh"
+
 namespace utils {
 
-    long long int two_pow(int power);
+    unsigned long long int two_pow(int power);
+    int pow_two(unsigned long long int number);
 
+    board::Position to_position(int x, int y);
 }
