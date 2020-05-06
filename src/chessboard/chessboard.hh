a1e469abe5c11a31373767fa6d7cf78f48f0ad71
clement.leconte <clement.leconte@epita.fr>
2020-05-06 21:00:12 +0200

    [UPDATE] Created the chessboard class

--- src/chessboard/chessboard.hh
+++ src/chessboard/chessboard.hh
@@ -1,3 +1,16 @@
+#pragma once
+
 #include "../all.hh"
 #include "../utils/all.hh"
 #include "../board/all.hh"
+#include "../bitboard/all.hh"
+
+namespace board {
+    class Chessboard {
+    public:
+        Chessboard();
+
+    private
+        Board board_;
+    }
+}
