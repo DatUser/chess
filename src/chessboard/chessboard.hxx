982c0dc1bad4d1c2fd3e66b07592e65a9c4d15d5
Diogo Simao Marques <diogo.simao-marques@epita.fr>
2020-05-13 14:54:46 +0200

    [FIX] Chessboard.hxx's syntax

--- src/chessboard/chessboard.hxx
+++ src/chessboard/chessboard.hxx
@@ -1,48 +1,47 @@
 #pragma once
 
 #include "chessboard.hh"
-
 Board Chessboard::getBoard()
 {
     return board_;
 }
 
-bool isWhiteTurn()
+bool Chessboard::isWhiteTurn()
 {
-    return white_turn;
+    return white_turn_;
 }
 
-bool getWhiteKingCastling()
+bool Chessboard::getWhiteKingCastling()
 {
     return white_king_castling_;
 }
 
-bool getWhiteQueenCastling()
+bool Chessboard::getWhiteQueenCastling()
 {
     return white_queen_castling_;
 }
 
-bool getBlackKingCastling()
+bool Chessboard::getBlackKingCastling()
 {
     return black_king_castling_;
 }
 
-bool getBlackQueenCastling()
+bool Chessboard::getBlackQueenCastling()
 {
     return black_queen_castling_;
 }
 
-Position getEnPassant()
+Position Chessboard::getEnPassant()
 {
-    return en_passant_;
+    return en_passant_.value();
 }
 
-unsigned int getTurn()
+unsigned int Chessboard::getTurn()
 {
     return turn_;
 }
 
-unsigned int getLastFiftyTurn()
+unsigned int Chessboard::getLastFiftyTurn()
 {
     return last_fifty_turn;
 }
