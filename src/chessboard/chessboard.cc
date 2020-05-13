30764aea20c158b78c6ddc39bf2978335a7ea711
Diogo Simao Marques <diogo.simao-marques@epita.fr>
2020-05-13 23:57:38 +0200

    [ADD] Skeleton for the generate_legal_moves

--- src/chessboard/chessboard.cc
+++ src/chessboard/chessboard.cc
@@ -1,4 +1,5 @@
 #include "chessboard.hh"
+#include "rule.hh"
 
 namespace board {
     Chessboard::Chessboard() {
@@ -11,5 +12,16 @@ namespace board {
             board_.do_move(move, Color::WHITE);
         else
             board_.do_move(move, Color::BLACK);
+    }
+
+
+    std::vector<Move> Chessboard::generate_legal_moves()
+    {
+        Rule rules = Rule();
+
+        std::vector<Move> result;
+        result = rules.generate_pawn_moves(*this);
+
+        return result;
     }
 }
