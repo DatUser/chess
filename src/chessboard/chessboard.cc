fd77ca6f88d1625b35f62a02a09d17a4e821f7e2
Diogo Simao Marques <diogo.simao-marques@epita.fr>
2020-05-14 10:24:24 +0200

    [ADD] Chessboard's generate_legal_moves

--- src/chessboard/chessboard.cc
+++ src/chessboard/chessboard.cc
@@ -15,13 +15,40 @@ namespace board {
     }
 
 
+    std::vector<Move> add_in_vector(std::vector<Move> v1, std::vector<Move> v2)
+    {
+        for (Move mv : v2)
+        {
+            v1.push_back(mv);
+        }
+
+        return v1;
+    }
+
+
     std::vector<Move> Chessboard::generate_legal_moves()
     {
         Rule rules = Rule();
+        std::vector<Move> all_moves;
 
-        std::vector<Move> result;
-        result = rules.generate_pawn_moves(*this);
+        std::vector<Move> pawns = rules.generate_pawn_moves(*this);
+        all_moves = add_in_vector(all_moves, pawns);
 
-        return result;
+        std::vector<Move> king = rules.generate_king_moves(*this);
+        all_moves = add_in_vector(all_moves, king);
+
+        std::vector<Move> bishops = rules.generate_bishop_moves(*this);
+        all_moves = add_in_vector(all_moves, bishops);
+
+        std::vector<Move> rooks = rules.generate_rook_moves(*this);
+        all_moves = add_in_vector(all_moves, rooks);
+
+        std::vector<Move> queen = rules.generate_queen_moves(*this);
+        all_moves = add_in_vector(all_moves, queen);
+
+        std::vector<Move> knights = rules.generate_knight_moves(*this);
+        all_moves = add_in_vector(all_moves, knights);
+
+        return all_moves;
     }
 }
