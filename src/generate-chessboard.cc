0822fbf51a530ca7912f3927c3d30f66469dfa88
Diogo Simao Marques <diogo.simao-marques@epita.fr>
2020-05-15 11:56:47 +0200

    [ADD] get_moves_from_pgn

--- src/generate-chessboard.cc
+++ src/generate-chessboard.cc
@@ -11,6 +11,22 @@ namespace board
 
         std::vector<Move> result;
 
+        for (PgnMove pm : pgn_moves)
+        {
+            Move mv = Move(pm.get_start(), pm.get_end());
+            mv.piece_set(pm.get_piece());
+            if (pm.get_promotion() != std::nullopt)
+            {
+                mv.promotion_set(pm.get_promotion().value());
+            }
+            if (pm.get_capture())
+            {
+                mv.capture_set(pm.get_piece());
+            }
+
+            result.push_back(mv);
+        }
+
         return result;
     }
 
