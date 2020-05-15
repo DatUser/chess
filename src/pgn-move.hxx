75a2dc7f9c25774bc1b52621215af5e1185216f1
Diogo Simao Marques <diogo.simao-marques@epita.fr>
2020-05-15 11:56:21 +0200

    [DELETE] Useless namespace

--- src/pgn-move.hxx
+++ src/pgn-move.hxx
@@ -1,32 +1,32 @@
 #pragma once
 #include "pgn-move.hh"
 
-    inline const Position PgnMove::get_start()
-    {
-        return start_;
-    }
+inline const Position PgnMove::get_start()
+{
+    return start_;
+}
 
-    inline const Position PgnMove::get_end()
-    {
-        return end_;
-    }
+inline const Position PgnMove::get_end()
+{
+    return end_;
+}
 
-    inline PieceType PgnMove::get_piece()
-    {
-        return piece_;
-    }
+inline PieceType PgnMove::get_piece()
+{
+    return piece_;
+}
 
-    inline PgnMove::opt_piece_t PgnMove::get_promotion()
-    {
-        return promotion_;
-    }
+inline PgnMove::opt_piece_t PgnMove::get_promotion()
+{
+    return promotion_;
+}
 
-    inline bool PgnMove::get_capture()
-    {
-        return capture_;
-    }
+inline bool PgnMove::get_capture()
+{
+    return capture_;
+}
 
-    inline ReportType PgnMove::get_report()
-    {
-        return report_;
-    }
+inline ReportType PgnMove::get_report()
+{
+    return report_;
+}
