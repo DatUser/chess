2a6d6a14de397245636ec1057562ca3f62263b57
Diogo Simao Marques <diogo.simao-marques@epita.fr>
2020-05-15 21:20:57 +0200

    [ADD] --pgn option treatment in the main

--- src/main.cc
+++ src/main.cc
@@ -2,6 +2,8 @@
 
 #include <parser/option-parser.hh>
 
+#include "generate-chessboard.hh"
+
 int main(int argc, char** argv) {
     OptionParser parser(argc, argv);
 
@@ -10,6 +12,11 @@ int main(int argc, char** argv) {
                   << "--pgn                     arg path to the PGN game file" << std::endl
                   << "-l [ --listeners ] args   list of paths to listener plugins" << std::endl
                   << "--perft arg               path to a perft file" << std::endl;
+    }
+    else if (parser.pgn_get() != "")
+    {
+        std::vector<board::Move> moves = board::get_moves_from_pgn(parser.pgn_get());
+        board::Chessboard chessboard = board::generate_chessboard(moves);
     }
 
     return 0;
