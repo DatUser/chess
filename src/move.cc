df9ed7cf5ca08f54e702b5fced909f3a4c2ae936
Diogo Simao Marques <diogo.simao-marques@epita.fr>
2020-05-18 11:57:04 +0200

    [ADD] Completed version of operator==

--- src/move.cc
+++ src/move.cc
@@ -7,11 +7,18 @@ namespace board
     : move_(std::pair<Position, Position>(start, end))
     {}
 
-    bool Move::operator==(const Move& move)
+    bool Move::operator==(Move& move)
     {
         std::pair<Position, Position> pos1 = move_get();
         std::pair<Position, Position> pos2 = move.move_get();
-        if (pos1.first == pos2.first and pos1.second == pos2.second)
+        if (pos1.first == pos2.first and pos1.second == pos2.second and
+            piece_get() == move.piece_get() and
+            promotion_get() == move.promotion_get() and
+            capture_get() == move.capture_get() and
+            double_pawn_push_get() == move.double_pawn_push_get() and
+            king_castling_get() == move.king_castling_get() and
+            queen_castling_get() == move.queen_castling_get() and
+            en_passant_get() == move.en_passant_get())
         {
             return true;
         }
