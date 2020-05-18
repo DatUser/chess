e7653fe037cae9579205a4a9082794ceb84bafc3
Diogo Simao Marques <diogo.simao-marques@epita.fr>
2020-05-18 11:42:44 +0200

    [ADD] Compiling first version of operator==

--- src/move.cc
+++ src/move.cc
@@ -1,10 +1,21 @@
 #include "all.hh"
 #include "move.hh"
 
-namespace board{
+namespace board
+{
+    Move::Move(Position start, Position end)
+    : move_(std::pair<Position, Position>(start, end))
+    {}
 
-Move::Move(Position start, Position end)
-: move_(std::pair<Position, Position>(start, end))
-{}
+    bool Move::operator==(const Move& move)
+    {
+        std::pair<Position, Position> pos1 = move_get();
+        std::pair<Position, Position> pos2 = move.move_get();
+        if (pos1.first == pos2.first and pos1.second == pos2.second)
+        {
+            return true;
+        }
 
+        return false;
+    }
 }
