6595be31f4a4b454324eeb54b7c7a8830ec38e73
Diogo Simao Marques <diogo.simao-marques@epita.fr>
2020-05-22 18:50:21 +0200

    [FIX] Compile error

--- src/move.cc
+++ src/move.cc
@@ -26,9 +26,28 @@ namespace board
         return false;
     }
 
-    Move Move::operator=(Move& move)
+    bool Move::operator==(const Move& move)
     {
-        if (move == *this)
+        std::pair<Position, Position> pos1 = move_get();
+        std::pair<Position, Position> pos2 = move.move_get();
+        if (pos1.first == pos2.first and pos1.second == pos2.second and
+            piece_get() == move.piece_get() and
+            promotion_get() == move.promotion_get() and
+            capture_get() == move.capture_get() and
+            double_pawn_push_get() == move.double_pawn_push_get() and
+            king_castling_get() == move.king_castling_get() and
+            queen_castling_get() == move.queen_castling_get() and
+            en_passant_get() == move.en_passant_get())
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    Move Move::operator=(const Move& move)
+    {
+        if (*this == move)
         {
             return move;
         }
