46ce2aba568eeb181f5a00b81212212c2eedf589
Diogo Simao Marques <diogo.simao-marques@epita.fr>
2020-05-22 18:50:05 +0200

    [ADD] const getters

--- src/move.hh
+++ src/move.hh
@@ -10,7 +10,7 @@ namespace board
         public:
             Move(Position start, Position end);
             ~Move() = default;
-            Move operator=(Move& move);
+            Move operator=(const Move& move);
             Move(const Move& move) = default;
 
             std::pair<Position, Position> move_get() const;
@@ -19,31 +19,48 @@ namespace board
 
             PieceType piece_get();
 
+            PieceType piece_get() const;
+
             void promotion_set(PieceType promotion);
 
             PieceType promotion_get();
+
+            PieceType promotion_get() const;
 
             void capture_set(PieceType capture);
 
             PieceType capture_get();
 
+            PieceType capture_get() const;
+
+
             void double_pawn_push_set(bool double_pawn_push);
 
             bool double_pawn_push_get();
+
+            bool double_pawn_push_get() const;
 
             void king_castling_set(bool king_castling);
 
             bool king_castling_get();
 
+            bool king_castling_get() const;
+
             void queen_castling_set(bool queen_castling);
 
             bool queen_castling_get();
+
+            bool queen_castling_get() const;
 
             void en_passant_set(bool en_passant);
 
             bool en_passant_get();
 
+            bool en_passant_get() const;
+
             bool operator==(Move& move);
+
+            bool operator==(const Move& move);
 
         private:
             const std::pair<Position, Position> move_;
